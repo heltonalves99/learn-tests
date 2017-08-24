779aa7a783a589feb51c21cb9c1d7cd0d8c7353c
Helton Alves <helton99@hotmail.com>
2017-08-24 00:21:11 -0300

    test 03-voter component, Angular.

--- angular/examples/src/app/app.module.ts
+++ angular/examples/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { TextSummaryPipePipe } from './01-text-summary-pipe/text-summary-pipe.pipe';
 import { LikeComponent } from './02-like/like.component';
+import { VoterComponent } from './03-voter/voter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TextSummaryPipePipe,
-    LikeComponent
+    LikeComponent,
+    VoterComponent
   ],
   imports: [
     BrowserModule,
