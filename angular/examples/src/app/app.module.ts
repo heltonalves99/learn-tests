e4dde5fd10e5cd858a800a45a3dfce4285327060
Helton Alves <helton99@hotmail.com>
2017-08-25 17:43:52 -0300

    Added 04-user component to make tests. o/

--- angular/examples/src/app/app.module.ts
+++ angular/examples/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { TextSummaryPipePipe } from './01-text-summary-pipe/text-summary-pipe.pipe';
 import { LikeComponent } from './02-like/like.component';
 import { VoterComponent } from './03-voter/voter.component';
+import { UserComponent } from './04-user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TextSummaryPipePipe,
     LikeComponent,
-    VoterComponent
+    VoterComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
