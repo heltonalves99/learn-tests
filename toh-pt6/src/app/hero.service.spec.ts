29cfbbf8d90cdbcdaff34ddb130bbf85b7e0103e
helton alves <helton99@hotmail.com>
2019-05-03 09:38:56 -0300

    Added more hero service tests.

--- toh-pt6/src/app/hero.service.spec.ts
+++ toh-pt6/src/app/hero.service.spec.ts
@@ -104,5 +104,177 @@ describe('Hero Service', () => {
         `HeroService: fetched hero id=${mockHero.id}`
       );
     });
+
+    it('should fail gracefully on error', () => {
+      heroService.getHero(mockHero.id).subscribe(
+        (hero: Hero) => expect(hero).toBeUndefined(),
+        fail,
+      );
+  
+      const req = httpTestingController.expectOne(`${heroesUrl}/${mockHero.id}`);
+      expect(req.request.method).toEqual('GET');
+      req.flush('Invalid request parameters', { status: 404, statusText: 'Bad Request' })
+  
+      expect((heroService as any).handleError).toHaveBeenCalledTimes(1);
+      expect((heroService as any).log).toHaveBeenCalledTimes(1);
+      expect((heroService as any).messageService.messages[0]).toEqual(
+        `HeroService: getHero id=${mockHero.id} failed: Http failure response for ${heroesUrl}/${mockHero.id}: 404 Bad Request`
+      )
+    })
+  });
+
+  describe('getHeroNo404', () => {
+    it('should return a single mock hero', () => {
+      heroService.getHeroNo404(mockHero.id).subscribe(
+        (hero: Hero) => expect(hero).toEqual(mockHero),
+        fail,
+      );
+      
+      const req = httpTestingController.expectOne(`${heroesUrl}/?id=${mockHero.id}`);
+      expect(req.request.method).toEqual('GET');
+      req.flush(mockHeroes);
+
+      expect((heroService as any).log).toHaveBeenCalledTimes(1);
+      expect((heroService as any).messageService.messages[0]).toEqual(
+        `HeroService: fetched hero id=${mockHero.id}`
+      );
+    });
+
+    it('should fail gracefully with undefined when id not found', () => {
+      heroService.getHeroNo404(123).subscribe(
+        (hero: Hero | undefined) => expect(hero).toBeUndefined(),
+        fail,
+      );
+
+      const req = httpTestingController.expectOne(`${heroesUrl}/?id=123`);
+      expect(req.request.method).toEqual('GET');
+      req.flush(mockHero);
+
+      expect((heroService as any).log).toHaveBeenCalledTimes(1);
+      expect((heroService as any).messageService.messages[0]).toEqual(
+        `HeroService: did not find hero id=123`
+      );
+    });
+
+    it('should fail gracefully on error', () => {
+      heroService.getHeroNo404(123).subscribe(
+        (hero: Hero | undefined) => expect(hero).toBeUndefined(),
+        fail,
+      );
+
+      const req = httpTestingController.expectOne(`${heroesUrl}/?id=123`);
+      expect(req.request.method).toEqual('GET');
+      req.flush('Invalid request parameters', { status: 404, statusText: 'Bad Request' });
+
+      expect((heroService as any).handleError).toHaveBeenCalledTimes(1);
+      expect((heroService as any).log).toHaveBeenCalledTimes(1);
+      expect((heroService as any).messageService.messages[0]).toEqual(
+        `HeroService: getHero id=123 failed: Http failure response for ${heroesUrl}/?id=123: 404 Bad Request`
+      );
+    });
+  });
+
+  describe('AddHero', () => {
+    it('should add a single Hero', () => {
+      heroService.addHero(mockHero).subscribe(
+        (hero: Hero) => expect(hero).toEqual(mockHero),
+        fail,
+      );
+
+      const req = httpTestingController.expectOne(heroesUrl);
+      expect(req.request.method).toEqual('POST');
+      req.flush(mockHero);
+
+      expect((heroService as any).log).toHaveBeenCalledTimes(1);
+      expect((heroService as any).messageService.messages[0]).toEqual(
+        `HeroService: added hero w/ id=${mockHero.id}`
+      );
+    });
+
+    it('should fail gracefully on error', () => {
+      heroService.addHero(mockHero).subscribe(
+        (hero: Hero | undefined) => expect(hero).toBeUndefined(),
+        fail,
+      );
+
+      const req = httpTestingController.expectOne(heroesUrl);
+      expect(req.request.method).toEqual('POST');
+      req.flush('Invalid request parameters', { status: 404, statusText: 'Bad Request' });
+
+      expect((heroService as any).handleError).toHaveBeenCalledTimes(1);
+      expect((heroService as any).log).toHaveBeenCalledTimes(1);
+      expect((heroService as any).messageService.messages[0]).toEqual(
+        'HeroService: addHero failed: Http failure response for api/heroes: 404 Bad Request'
+      );
+    });
+  });
+
+  describe('UpdateHero', () => {
+    it('should update hero', () => {
+      heroService.updateHero(mockHero).subscribe(
+        (hero: Hero) => expect(mockHero).toEqual(mockHero),
+        fail,
+      );
+
+      const req = httpTestingController.expectOne(heroesUrl);
+      expect(req.request.method).toEqual('PUT');
+      req.flush(mockHero);
+
+      expect((heroService as any).log).toHaveBeenCalledTimes(1);
+      expect((heroService as any).messageService.messages[0]).toEqual(
+        `HeroService: updated hero id=${mockHero.id}`
+      );
+    });
+
+    it('should fail gracefully on error', () => {
+      heroService.updateHero(mockHero).subscribe(
+        (hero: Hero | undefined) => expect(hero).toBeUndefined(),
+        fail,
+      );
+      
+      const req = httpTestingController.expectOne(heroesUrl);
+      expect(req.request.method).toEqual('PUT');
+      req.flush('Invalid request parameters', { status: 404, statusText: 'Bad Request'});
+
+      expect((heroService as any).handleError).toHaveBeenCalledTimes(1);
+      expect((heroService as any).log).toHaveBeenCalledTimes(1);
+      expect((heroService as any).messageService.messages[0]).toEqual(
+        `HeroService: updateHero failed: Http failure response for ${heroesUrl}: 404 Bad Request`
+      );
+    });
+  });
+
+  describe('DeleteHero', () => {
+    it('should delete hero using id', () => {
+      heroService.deleteHero(mockHero.id).subscribe(
+        (response: Hero | number) => expect(response).toEqual(mockHero.id),
+        fail,
+      );
+
+      const req = httpTestingController.expectOne(`${heroesUrl}/${mockHero.id}`);
+      expect(req.request.method).toEqual('DELETE');
+      req.flush(mockHero.id);
+
+      expect((heroService as any).log).toHaveBeenCalledTimes(1);
+      expect((heroService as any).messageService.messages[0]).toEqual(
+        `HeroService: deleted hero id=${mockHero.id}`
+      );
+    });
+
+    it('should delete hero using hero object', () => {
+      heroService.deleteHero(mockHero).subscribe(
+        (response: Hero | number) => expect(response).toEqual(mockHero.id),
+        fail,
+      );
+
+      const req = httpTestingController.expectOne(`${heroesUrl}/${mockHero.id}`);
+      expect(req.request.method).toEqual('DELETE');
+      req.flush(mockHero.id);
+
+      expect((heroService as any).log).toHaveBeenCalledTimes(1);
+      expect((heroService as any).messageService.messages[0]).toEqual(
+        `HeroService: deleted hero id=${mockHero.id}`
+      );
+    });
   });
 });
